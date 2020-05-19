4616a3d2abb50386110a442468b50d9172f18de5
Charlson Kent Salazar <charlsonkent@gmail.com>
2020-05-19 16:19:39 +0800

    remove todos.vue

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,28 @@
 <template>
   <div id="app">
-   <Todos />
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import Todos from "./components/Todos";
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    Todos
-  },
-  data(){
-    return{
-      todos:{
-        {
-          id:1,
-          title: "Todo One",
-          completed: false
-        },
-        {
-          id:2,
-          title: "Todo Two",
-          completed: true
-        },
-        {
-          id:2,
-          title: "Todo Two",
-          completed: false
-        }
-      }
-    }
+    HelloWorld
   }
 }
 </script>
 
 <style>
-    * {
-      box-sizing: border-box;
-      margin: 0;
-      padding: 0;
-    }
-    body {
-        font-family: Arial, Arial, Helvetica, sans-serif;
-        line-height: 1.4;
-    }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
